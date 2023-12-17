492aa2cd4e02ae2af8a4f683115d1df610aa47f8
yigitkalan <sametykalan@gmail.com>
2023-12-17 19:52:25 +0300

    try fix

--- Assets/IRONHEAD Games/Lab/SpawnManager.cs
+++ Assets/IRONHEAD Games/Lab/SpawnManager.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class SpawnManager : MonoBehaviour
+public class SpawnManager :  MonoBehaviourPunCallbacks
 {
     [SerializeField]
     GameObject genericVRPlayerPrefab;
@@ -14,7 +13,16 @@ public class SpawnManager : MonoBehaviour
         //this means we are connected photon and do some operations like join or leave 
         //now we can instantiate the players. 
 
-        PhotonNetwork.Instantiate(genericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
+        // PhotonNetwork.Instantiate(genericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if(PhotonNetwork.IsMasterClient){
+            Debug.Log("OnPlayerEnteredRoom() " + newPlayer.NickName);
+            if(PhotonNetwork.IsMasterClient){
+                PhotonNetwork.Instantiate(genericVRPlayerPrefab.name, spawnPosition, Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
